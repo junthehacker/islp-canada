ae73699b8cbcdae70e92244184382f83883c54e6
Jun Zheng <me@jackzh.com>
2018-09-06 16:21:17 -0400

    Update build number

--- resources/views/portal/partials/version.blade.php
+++ resources/views/portal/partials/version.blade.php
@@ -1,4 +1,4 @@
 <div class="container-fluid footer">
     <hr>
-    <p class="small text-muted">ISLP Dashboard, Version 1.1.0 Build 0004</p>
+    <p class="small text-muted">ISLP Dashboard, Version 1.1.0 Build 0005</p>
 </div>
