b6bd9b157593a6cef193f911d958593ce3e2da56
Jun Zheng <me@jackzh.com>
2018-09-18 09:55:03 -0400

    Upgrade Laravel to 5.7

--- resources/views/portal/partials/version.blade.php
+++ resources/views/portal/partials/version.blade.php
@@ -1,4 +1,4 @@
 <div class="container-fluid footer">
     <hr>
-    <p class="small text-muted">ISLP Dashboard, Version 1.1.0 Build 0005</p>
+    <p class="small text-muted">ISLP Dashboard, Version 1.1.0 Build 0007</p>
 </div>
